4fb5b1d35ff9c7d746a5c8650bc6481e996b3c8c
WALTER-OBS-DEBUG <jonasdowalter@gmail.com>
2021-09-11 00:51:43 -0400

    DataTable integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,3 +1,28 @@
+import { seller } from "./seller"
+
+
+export type Sale =  {
+    id: number,
+    visited: number,
+    deals: number,
+    amount:number,
+    date: string,
+    seller: seller
+}
+
+
+export type SalePage = {
+    content?: Sale[];
+    last: boolean,
+    totalElements: number,
+    totalPages: number,
+    size?: number,
+    number: number,
+    first: boolean,
+    numberOfElements?: number,
+    empty?: boolean,
+}
+
 export type SaleSum =  {
     sellerName: string;
     sum: number;
---
+++ frontend/src/types/seller.ts
@@ -0,0 +1,4 @@
+export type seller = {
+    id: number,
+    name: string
+}
